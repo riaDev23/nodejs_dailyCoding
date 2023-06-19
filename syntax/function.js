a846a4d89176d37bfaca83a2ba49400df863260a
riadev23 <riadev@proton.me>
2023-06-19 20:52:46 +0900

    daliy coding

---
+++ syntax/function.js
@@ -0,0 +1,10 @@
+f123();
+
+function f123() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+}
+
+
+console.log("A");``
---
+++ syntax/function2.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.65)); //2
+console.log(Math.round(1.23)); //1
+
+
+function sum(first, second) { // parameter
+  return `${first}${second}`;
+}
+
+console.log(sum(2,4)); //argument
